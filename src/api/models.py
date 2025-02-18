d0ea78c7af1795d97fd44faf96d3d2958dd1ea59
alipereira19 <aliper1908@gmail.com>
2025-02-18 04:45:26 +0000

    un par de modelos agregados

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 db = SQLAlchemy()
 
@@ -17,3 +18,45 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class User_principal(db.Model):
+    __tablename__ = 'user_principal'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    general_data_id = db.Column(db.Integer, db.ForeignKey("general_data.id"), nullable=False )
+
+    def __repr__(self):
+        return f'<User_principal {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
+class General_data(db.Model):
+    __tablename__ = 'general_data'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    first_last_name = db.Column(db.String(80), unique=False, nullable=False)
+    second_last_name = db.Column(db.String(80), unique=False, nullable=False)
+    nacionality = db.Column(db.String(80), unique=False, nullable=False)
+    gender = db.Column(db.String(80), unique=False, nullable=False)
+    birthdate = db.Column(db.Date, nullable=True)
+
+    def __repr__(self):
+        return f'<General_data {self.id}>' #Se cambia a id, ya que email no existe.
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "first_last_name": self.first_last_name,
+            "second_last_name": self.second_last_name,
+            "nacionality": self.nacionality,
+            "gender": self.gender,
+            "birthdate": self.birthdate.isoformat() if self.birthdate else None #Formatea la fecha si existe, sino retorna None.
+        }
+
