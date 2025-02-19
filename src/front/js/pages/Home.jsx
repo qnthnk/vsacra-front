8ff05d80d256b9aff2e9dd511bba9f87948e2bb7
Ruben <167398241+rumacon@users.noreply.github.com>
2025-02-19 02:51:38 +0000

    Register view lista

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div class="d-flex justify-content-center align-items-center vh-100">
-      <ul class="list-group w-auto">
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center" aria-current="true">Lugares de ayuda</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Embajadas</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Mensajeria</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Blog</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Gadgets</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Chatbot</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Contact List</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Requisitos migratorios</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Saldo paypal</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Preguntas Frecuentes</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Emergencia</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Estadisticas y reportes</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Contactar Ayuda</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Vista de ubicacion</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Consola de manejo de datos</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Panel de edicion de dasborad de usuarios</li>
-        <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Compartir</li>
+    <div>
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <ul className="list-group w-auto">
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center" aria-current="true">Lugares de ayuda</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Embajadas</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Mensajeria</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Blog</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Gadgets</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Chatbot</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Contact List</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Requisitos migratorios</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Saldo paypal</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Preguntas Frecuentes</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Emergencia</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Estadisticas y reportes</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Contactar Ayuda</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Vista de ubicacion</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Consola de manejo de datos</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Panel de edicion de dasborad de usuarios</li>
+          <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center">Compartir</li>
 
-      </ul>
+        </ul>
+      </div>
     </div>
   )
 }
