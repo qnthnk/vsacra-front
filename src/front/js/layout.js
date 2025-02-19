389f4db242f3f5a7a36cfaac86b7c324b21b527c
PorrasD <122325175+PorrasD@users.noreply.github.com>
2025-02-19 02:29:04 +0000

    navbar funcional  y vistas navegables basicas

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,12 +2,24 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
-import Home from "./pages/Home.jsx"
-import Login from "./pages/Login.jsx"
-import Register from "./pages/Register.jsx"
-import Contact from "./pages/Contact.jsx"
-import ContactList from "./pages/Contact_list.jsx"
+import Lugares_de_ayuda from './pages/Lugares_de_ayuda.jsx';
+import Embajadas from './pages/Embajadas.jsx';
+import Mensajeria from './pages/Mensajeria.jsx';
+import Blog from './pages/Blog.jsx';
+import Gadgets from './pages/Gadgets.jsx';
+import Chatbot from './pages/Chatbot.jsx';
+import Contact_list from './pages/Contact_list.jsx';
+import Requisitos_migratorios from './pages/Requisitos_migratorios.jsx';
+import Saldo_paypal from './pages/Saldo_paypal.jsx';
+import Preguntas_frecuentes from './pages/Preguntas_frecuentes.jsx';
+import Emergencia from './pages/Emergencia.jsx';
+import Estadisticas_reportes from './pages/Estadisticas_reportes.jsx';
+import Contactar_ayuda from './pages/Contactar_ayuda.jsx';
+import Vista_ubicacion from './pages/Vista_ubicacion.jsx';
+import Consola_manejo_de_datos from './pages/Consola_manejo_de_datos.jsx';
+import Panel_edicion_dashboard from './pages/Panel_edicion_dashboard.jsx';
 import injectContext from "./store/appContext";
+import Home from "./pages/Home.jsx";
 import Navbar from "./component/Navbar.jsx";
 import Footer from "./component/Footer.jsx";
 
@@ -17,7 +29,7 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
     return (
         <div>
@@ -25,12 +37,23 @@ const Layout = () => {
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Login />} path="/login" />
-                        <Route element={<Register />} path="/register" />
-                        <Route element={<ContactList />} path="/contactlist" />
-                        <Route element={<Contact />} path="/contact" />
-                        <Route element={<h1>Not found!</h1>} />
+                        <Route path="/" element={<Home/>} />
+                        <Route path="/lugares-de-ayuda" element={<Lugares_de_ayuda />} />
+                        <Route path="/embajadas" element={<Embajadas />} />
+                        <Route path="/mensajeria" element={<Mensajeria />} />
+                        <Route path="/blog" element={<Blog />} />
+                        <Route path="/gadgets" element={<Gadgets />} />
+                        <Route path="/chatbot" element={<Chatbot />} />
+                        <Route path="/contact-list" element={<Contact_list />} />
+                        <Route path="/requisitos-migratorios" element={<Requisitos_migratorios />} />
+                        <Route path="/saldo-paypal" element={<Saldo_paypal />} />
+                        <Route path="/preguntas-frecuentes" element={<Preguntas_frecuentes />} />
+                        <Route path="/emergencia" element={<Emergencia />} />
+                        <Route path="/estadisticas-reportes" element={<Estadisticas_reportes />} />
+                        <Route path="/contactar-ayuda" element={<Contactar_ayuda />} />
+                        <Route path="/vista-ubicacion" element={<Vista_ubicacion />} />
+                        <Route path="/consola-manejo-datos" element={<Consola_manejo_de_datos />} />
+                        <Route path="/panel-edicion-dashboard" element={<Panel_edicion_dashboard />} />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
