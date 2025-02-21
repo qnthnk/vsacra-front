96e87f385979f6eb4d34a8b381441aaecc3953e3
Ruben <167398241+rumacon@users.noreply.github.com>
2025-02-21 23:45:45 +0000

    ubicacion en home

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useEffect, useContext, useState } from 'react'
+import { Context } from '../store/appContext';
 import { Link } from "react-router-dom";
 
 
+
+
 const Home = () => {
+  const { store, actions } = useContext(Context);
+ 
+    const [location, setLocation] = useState(null);
+    const [error, setError] = useState(null);
+  
+
+    useEffect(() => {
+      if (!navigator.geolocation) {
+        setError("La geolocalización no es soportada por este navegador.");
+        return;
+      }
+  
+      const getLocation = () => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            setLocation({ latitud: latitude, longitud: longitude });
+            console.log("ubiv", location);
+          },
+          (error) => {
+            setError("Error al obtener la ubicación: ", error);
+          }
+        );
+      };
+  
+      getLocation();
+    }, []);
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
+
+<div>
+      <h2>Ubicación del Usuario</h2>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {location ? (
+        <p>
+          Latitud: {location.latitud}, Longitud: {location.longitud}
+          <h2>AHUEVOOOO</h2>
+        </p>
+        
+      ) : (
+        <p>Obteniendo ubicación...</p>
+      )}
+    </div>
       <ul className="list-group w-auto">
         <li className="list-group-item list-group-item-action d-flex justify-content-center align-items-center" aria-current="true">
           <Link to="/register">Registro</Link>
