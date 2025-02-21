5bf0c7972bf37c66fa0ec56e58c2258935a66c6b
Ruben <167398241+rumacon@users.noreply.github.com>
2025-02-21 04:36:58 +0000

    avance dashadmin

---
+++ src/front/js/component/DashboardAdminItems.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const DashboardAdminItems = () => {
+  return (
+  <div className='card bg-dark' style={{width:"30%"}}>
+    <div className="grid gap-0 column-gap-3">
+  <div className="p-2 g-col-6" style={{height: "100px"}}>Chart 1</div>
+  <div className="p-2 g-col-6" style={{height: "100px"}}>Chart 2</div>
+  <div className="p-2 g-col-6" style={{height: "100px"}}>Chart 3</div>
+    </div>
+</div> 
+  )
+};
+
+export default DashboardAdminItems
